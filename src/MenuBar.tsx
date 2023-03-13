c2763d0454c9528da2e68b4b2563954eb11d99ce
Richard Li <richprli@amazon.com>
2023-03-12 23:10:36 -0700

    added trivia as part of menu, as disabled

--- src/MenuBar.tsx
+++ src/MenuBar.tsx
@@ -17,6 +17,9 @@ function MenuBar() {
           <Nav className="me-auto" activeKey={location.pathname}>
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/trivia" disabled>
+              Trivia
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
